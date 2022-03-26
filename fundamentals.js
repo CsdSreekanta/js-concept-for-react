4315dbafc32c510704eb5cf71a8fbb54cc085f8a
CsdSreekanta <sreekanta.csd@gmail.com>
2022-03-26 17:20:23 +0600

    foundamental.js added

---
+++ fundamentals.js
@@ -0,0 +1,9 @@
+const student={
+    name: 'sakib',
+    age :34,
+    movies: ['mayer doya', 'sokinar jamai']
+}
+const myAge ='age'
+console.log(student.age);
+console.log(student['age']);
+console.log(student[myAge]);
